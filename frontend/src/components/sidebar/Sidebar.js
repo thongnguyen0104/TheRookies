0dd6a9de08e7cc34797cc83ece8694091b03dd86
thongnguyen0104 <nguyenhoangthong0104@gmail.com>
2022-07-13 17:23:33 +0700

    Add Components

---
+++ frontend/src/components/sidebar/Sidebar.js
@@ -0,0 +1,99 @@
+import { useEffect, useRef, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './Sidebar.scss';
+import AppLayout from '../../layout/AppLayout';
+import React from 'react'
+
+const sidebarNavItems = [
+    {
+        display: 'Dashboard',
+        icon: <i className='bx bx-home'></i>,
+        to: '/',
+        section: ''
+    },
+    {
+        display: 'Products',
+        icon: <i className='bx bx-star'></i>,
+        to: '/products',
+        section: 'products'
+    },
+    {
+        display: 'ProductTypes',
+        icon: <i className='bx bx-calendar'></i>,
+        to: '/producttypes',
+        section: 'producttypes'
+    },
+    {
+        display: 'Users',
+        icon: <i className='bx bx-user'></i>,
+        to: '/users',
+        section: 'users'
+    },
+    {
+        display: 'Orders',
+        icon: <i className='bx bx-receipt'></i>,
+        to: '/order',
+        section: 'order'
+    },
+]
+
+const Sidebar = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [stepHeight, setStepHeight] = useState(0);
+    const sidebarRef = useRef();
+    const indicatorRef = useRef();
+    const location = useLocation();
+
+    useEffect(() => {
+        setTimeout(() => {
+            const sidebarItem = sidebarRef.current.querySelector('.sidebar__menu__item');
+            indicatorRef.current.style.height = `${sidebarItem.clientHeight}px`;
+            setStepHeight(sidebarItem.clientHeight);
+        }, 50);
+    }, []);
+
+    // change active index
+    useEffect(() => {
+        const curPath = window.location.pathname.split('/')[1];
+        const activeItem = sidebarNavItems.findIndex(item => item.section === curPath);
+        setActiveIndex(curPath.length === 0 ? 0 : activeItem);
+    }, [location]);
+    
+    return (
+        <div className='sidebar'>
+            <div className="sidebar__logo">
+                Animate
+            </div>
+            <div ref={sidebarRef} className="sidebar__menu">
+                <div
+                    ref={indicatorRef}
+                    className="sidebar__menu__indicator"
+                    style={{
+                        transform: `translateX(-50%) translateY(${activeIndex * stepHeight}px)`
+                    }}
+                ></div>
+                {
+                    sidebarNavItems.map((item, index) => (
+                        <Link
+                            to={item.to}
+                            key={index}
+                            // onClick={AppLayout(item.display)}
+                        >
+                            <div className={`sidebar__menu__item ${activeIndex === index ? 'active' : ''}`}>
+                                <div className="sidebar__menu__item__icon">
+                                    {item.icon}
+                                </div>
+                                <div className="sidebar__menu__item__text">
+                                    {item.display}
+                                </div>
+                            </div>
+                        </Link>
+                    ))
+                }
+            </div>
+        </div>
+    );
+
+};
+
+export default Sidebar;
