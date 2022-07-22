9f982a5b05c42737faefef423ad76f5d0e2f190b
thongnguyen0104 <nguyenhoangthong0104@gmail.com>
2022-07-22 23:40:56 +0700

    Update Frontend App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppLayout from './layout/AppLayout';
+import Blank from './views/pages/Blank';
+import Products from './views/products';
+import ProductTypes from './views/productTypes/index.js';
+import Users from './views/users';
+import FormExample from './views/products/create'
+import EditProduct from './views/products/edit';
+import CreateProuctType from './views/productTypes/create'
+import EditProductType from './views/productTypes/edit'
+import UploadImg from './views/products/upload';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AppLayout />}>
+          <Route index element={<Blank />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/producttypes' element={<ProductTypes />} />
+          <Route path='/producttypes/create' element={<CreateProuctType />} />
+          <Route path='/producttypes/:id/edit' element={<EditProductType />} />
+          <Route path='/users' element={<Users />} />
+          <Route path='/products/create' element={<FormExample />} />
+          <Route path='/products/:id/edit' element={<EditProduct />} />
+          <Route path='/products/:id/updateImage' element={<UploadImg />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    // <AppLayout/>
   );
 }
 
