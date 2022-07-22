c353c1fc4fee2ee7e7691c07c7d87c193662e95c
thongnguyen0104 <nguyenhoangthong0104@gmail.com>
2022-07-22 23:49:02 +0700

    Add Components RazorPage

---
+++ Economic/Economic.RazorPage/Pages/Shared/Components/ProductType/ProductType.cs
@@ -0,0 +1,25 @@
+﻿using Economic.RazorPage.Integration;
+using Economic.RazorPage.Models;
+using Economic.ViewModels.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Refit;
+
+namespace Economic.RazorPage.Pages.Shared.Components.ProductType
+{
+    public class ProductType : ViewComponent
+    {
+        private IProductTypeApi _productTypeApi;
+        
+        public ProductType()
+        {
+            _productTypeApi = RestService.For<IProductTypeApi>("https://localhost:7246");
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var productTypes = _productTypeApi.GetAllAsync().GetAwaiter().GetResult();
+
+            return View<List<ProductTypeViewModel>>(productTypes);
+        }
+    }
+}
