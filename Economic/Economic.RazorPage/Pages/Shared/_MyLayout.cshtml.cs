d5b5c3877f1aa05b2a68089b76ea8704ee8e9eae
thongnguyen0104 <nguyenhoangthong0104@gmail.com>
2022-07-22 23:49:46 +0700

    Add MyLayout RazorPage

---
+++ Economic/Economic.RazorPage/Pages/Shared/_MyLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Economic.RazorPage.Pages.Shared
+{
+    public class _MyLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
