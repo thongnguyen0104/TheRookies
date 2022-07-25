57c558357241140ebd3d88b65a154e3a3a49e683
thongnguyen0104 <nguyenhoangthong0104@gmail.com>
2022-07-25 12:00:40 +0700

    Add Test Service

---
+++ Economic/Economic.UnitTest/FluentValidation/FluentValidationTests.cs
@@ -0,0 +1,23 @@
+﻿using Economic.Data.Configurations;
+using Economic.ViewModels.Request.ProductType;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Economic.UnitTest.FluentValidation
+{
+    [TestClass]
+    public class FluentValidationTests
+    {
+        private ProductTypeCreateRequest productTypeCreateRequest;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            productTypeCreateRequest = new ProductTypeCreateRequest();
+        }
+    }
+}
